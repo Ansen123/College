3a10ee9d0a80c6b588bcb3db08b04fb6d3ccfb1b
Ansen123 <ansenfp@gmail.com>
2022-10-07 12:24:40 +0530

    component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddstudentComponent } from './addstudent/addstudent.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddstudentComponent
+    AddstudentComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
